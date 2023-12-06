bb85c1478f6eb25f4ef380ce5d05627c9419cdc6
GrimTaleTV <103109643+GrimTaleTV@users.noreply.github.com>
2023-12-06 16:32:22 +0100

    Asteroid_Scene and vignette
    I Cant think right now

---
+++ Assets/Vignette/VignetteController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VignetteController : MonoBehaviour
+{
+    // A reference to the TunnelingVignetteController component
+    public TunnelingVignetteController tunnelingVignetteController;
+
+    // A public variable to control the vignette size
+    [Range(0f, 1f)]
+    public float vignetteSize;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the vignette size to the public variable value
+        tunnelingVignetteController.vignetteSize = vignetteSize;
+    }
+}
