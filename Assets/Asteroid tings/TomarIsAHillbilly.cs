381bfc2036d4e6bcf44135d7aa327bfc8861cad6
GrimTaleTV <103109643+GrimTaleTV@users.noreply.github.com>
2023-12-08 11:44:16 +0100

    AsteroidDone

--- Assets/Asteroid tings/TomarIsAHillbilly.cs
+++ Assets/Asteroid tings/TomarIsAHillbilly.cs
@@ -1,20 +1,55 @@
+using JetBrains.Annotations;
+using nickmaltbie.ScrollingShader;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TomarIsAHillbilly : MonoBehaviour
 {
+    // A reference to the other component
+    public AsteroidSpawner asteroidspawner;
+    public ConveyerBelt ConveyerBelt;
+
     float Zackie = 0;
+    public GameObject startButton;
+    public GameObject spawner;
+    public TMP_Text timerText;
+    public TMP_Text score;
+    public TMP_Text Announcer;
+    private float countdownTime = 0;
+    public float messageInterval = 2f;
+    bool isRunning = false;
+    bool gamehaps = false;
+    private string[] messages = {
+        "Welcome.",
+        "Hit the rocks.",
+        "Deposit gems.",
+        "Good Luck!"
+        // Add more messages as needed
+    };
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gamehaps && countdownTime > 0)
+        {
+            countdownTime -= Time.deltaTime; // Countdown by deltaTime (time since the last frame)
+            DisplayTime(countdownTime);
+        }
+        else
+        {
+            countdownTime = 0; // To prevent displaying negative values
+            gameEnd();
+            // Optionally, you can trigger an action when the countdown reaches zero
+            // For example: TimerEnded();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -32,4 +67,117 @@ public class TomarIsAHillbilly : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+
+    public void startAsteroidgame()
+    {
+        introduction();
+        startButton.SetActive(false);
+    }
+
+    void introduction()
+    {
+        StartCoroutine(ShowMessages());
+        StartCoroutine(GameStarter());
+    }
+   
+    void gameStart()
+    {
+        score.text = Zackie.ToString();
+        timerText.text = countdownTime.ToString();
+        spawner.SetActive(true);
+        countdownTime = 60f;
+        gamehaps = true;
+        difficultyOne();
+        difficultyTwo();
+        difficultyThree();
+
+    }
+    void gameEnd()
+    {
+        spawner.SetActive(false);
+        startButton.SetActive(true);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        // Convert timeToDisplay to minutes and seconds
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        // Update the UI Text element to display the timer in MM:SS format
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    IEnumerator ShowMessages()
+    {
+        foreach (string message in messages)
+        {
+            Announcer.text = message;
+            yield return new WaitForSeconds(messageInterval);
+        }
+    }
+
+    IEnumerator GameStarter()
+    {
+        isRunning = true;
+
+        // Your initial function logic goes here
+        Debug.Log("FirstFunction started.");
+
+        yield return new WaitForSeconds(9f); // Wait for 9 seconds
+
+        if (isRunning)
+        {
+            gameStart(); // Activate the second function
+        }
+    }
+
+    IEnumerator difficultyOne()
+    {
+        isRunning = true;
+
+        // Your initial function logic goes here
+        Debug.Log("FirstFunction started.");
+
+        yield return new WaitForSeconds(0.1f); // Wait for x seconds
+
+        if (isRunning)
+        {
+            asteroidspawner.spawnInterval = 2f;
+            ConveyerBelt.velocity = 1f;    // Activate the second function
+        }
+    }
+
+    IEnumerator difficultyTwo()
+    {
+        isRunning = true;
+
+        // Your initial function logic goes here
+        Debug.Log("FirstFunction started.");
+
+        yield return new WaitForSeconds(30f); // Wait for x seconds
+
+        if (isRunning)
+        {
+            asteroidspawner.spawnInterval = 1.5f;
+            ConveyerBelt.velocity = 2f;    // Activate the second function
+        }
+    }
+
+    IEnumerator difficultyThree()
+    {
+        isRunning = true;
+
+        // Your initial function logic goes here
+        Debug.Log("FirstFunction started.");
+
+        yield return new WaitForSeconds(45f); // Wait for x seconds
+
+        if (isRunning)
+        {
+            asteroidspawner.spawnInterval = 1f;
+            ConveyerBelt.velocity = 3f;    // Activate the second function
+        }
+    }
+
 }
